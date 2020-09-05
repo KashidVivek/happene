37d9478156cae4b50c4b6d258fae738fbb0432e5
Vivek Lalasaheb Kashid <vivek@Viveks-MacBook-Pro.local>
2020-09-04 20:32:46 -0400

    Added Search bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
-import { Map, Marker, Popup, TileLayer, MapControl, withLeaflet } from "react-leaflet";
+import { Map, Marker, Popup, TileLayer, MapControl, withLeaflet, useLeaflet } from "react-leaflet";
 import { Icon } from "leaflet";
 import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
 import './App.css';
+import './leaflet-search.css'
+
+// const Search = (props) => {
+//     const { map } = useLeaflet() // access to leaflet map
+//     const { provider } = props
+//
+//     useEffect(() => {
+//         const searchControl = new GeoSearchControl({
+//             provider: new OpenStreetMapProvider(),
+//             // style: 'bar',
+//         })
+//
+//         map.addControl(searchControl) // this is how you add a control in vanilla leaflet
+//         return () => map.removeControl(searchControl)
+//     }, [props])
+//
+//     return null
+// }
 
 class SearchMap extends MapControl {
     createLeafletElement() {
@@ -15,12 +33,24 @@ class SearchMap extends MapControl {
         autoClose: true,
         retainZoomLevel: false,
         animateZoom: true,
-        keepResult: false,
+        keepResult: true,
         searchLabel: 'search'
       });
     }
 }
 
+// function App(){
+//   return (
+//     <Map center={[40.61, -74.03]} zoom={12}>
+//       <Search />
+//       <TileLayer
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//       />
+//
+//     </Map>
+//   );
+// }
 class App extends React.Component {
   render(){
   const SearchBar = withLeaflet(SearchMap);
